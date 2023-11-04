838fa045881b3eb30022c8627715d522cfc11145
Thao <phuongthao120303@gmail.com>
2023-11-04 22:33:06 +0700

    Add p31.php file

---
+++ p31.php
@@ -0,0 +1,5 @@
+<?php
+echo "Repository (kho chứa) nghĩa là nơi mà bạn sẽ lưu trữ mã nguồn và một người khác có thể sao chép (clone) lại mã nguồn đó nhằm làm việc. Repository có hai loại là:
+Local Repository (Kho chứa trên máy cá nhân)
+Remote Repository (Kho chứa trên một máy chủ từ xa).";
+?>
